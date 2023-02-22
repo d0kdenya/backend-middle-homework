7a48653fa4fb809896070b1826d58a2511273c05
Dan Losev <losev_da@nntu.ru>
2023-02-22 23:59:36 +0300

    express one

--- config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  'myAPIKEY': '0970a2f45dfc5d829d85754b45184cfa'
-}
--- index.js
+++ index.js
@@ -1,34 +1,98 @@
-#!/usr/bin/env node
-require('dotenv').config()
+const express = require('express')
+const { v4: uuid } = require('uuid')
 
-const http = require('http')
-const myAPIKEY = process.env.myAPIKEY;
-let country = process.argv[2];
-
-if (process.argv.length === 2) {
-  country = 'Moscow'
-} else {
-  for (let i = 2; i < process.argv.length; i++) {
-    country += '%20' + process.argv[i]
+class Book {
+  constructor(title = '', description = '', authors = '', favorite = '', fileCover = '', fileName = '', id = uuid()) {
+    this.id = id
+    this.title = title
+    this.description = description
+    this.authors = authors
+    this.favorite = favorite
+    this.fileCover = fileCover
+    this.fileName = fileName
   }
 }
-const url = `http://api.weatherstack.com/current?access_key=${ myAPIKEY }&query=${ country }`
 
-http.get(url, res => {
-  const { statusCode } = res
-  if (statusCode !== 200) {
-    console.log(`statusCode: ${ statusCode }`)
-    return 0
-  }
-  res.setEncoding('utf8')
+const library = {
+  book: []
+}
 
-  let rowData = ''
+const app = express()
 
-  res.on('data', chunk => rowData += chunk)
-  res.on('end', () => {
-    let parseData = JSON.parse(rowData)
-    console.log(parseData)
-  })
-}).on('error', err => {
-  console.log(err)
+app.use(express.json())
+
+app.get('/api/books', (req, res) => {
+  const { book } = library
+  res.json(book)
 })
+app.get('/api/books/:id', (req, res) => {
+  const { book } = library
+  const { id } = req.params
+  const idx = book.findIndex(el => el.id === id)
+
+  if (idx !== -1) {
+    res.json(book[idx])
+  } else {
+    res.status(404)
+    res.json('404 | Страница не найдена!')
+  }
+})
+
+app.post('/api/user/login', (req, res) => {
+  res.status(201)
+  res.json({
+    id: 1,
+    mail: 'test@mail.ru'
+  })
+})
+app.post('/api/books', (req, res) => {
+  const { book } = library
+  const { title, description, authors, favorite, fileCover, fileName } = req.body
+
+  const newBook = new Book(title, description, authors, favorite, fileCover, fileName)
+  book.push(newBook)
+
+  res.status(201)
+  res.json(newBook)
+})
+
+app.put('/api/books/:id', (req, res) => {
+  const { book } = library
+  const { id } = req.params
+  const { title, description, authors, favorite, fileCover, fileName } = req.body
+  const idx = book.findIndex(el => el.id === id)
+
+  if (idx !== -1) {
+    book[idx] = {
+      ...book[idx],
+      title,
+      description,
+      authors,
+      favorite,
+      fileCover,
+      fileName
+    }
+    res.json(book[idx])
+  } else {
+    res.status(404)
+    res.json('404 | Страница не найдена!')
+  }
+})
+app.delete('/api/books/:id', (req, res) => {
+  const { book } = library
+  const { id } = req.params
+  const idx = book.findIndex(el => el.id === id)
+
+  if (idx !== -1) {
+    book.splice(idx, 1)
+    res.json('ok')
+  } else {
+    res.status(404)
+    res.json('404 | Страница не найдена!')
+  }
+})
+
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => console.log(`Server started on ${ PORT } port!`))
