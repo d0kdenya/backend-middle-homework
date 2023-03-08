376035899ee0ddb6a5f9e0d64b8f399eab090452
Dan Losev <losev_da@nntu.ru>
2023-03-08 23:38:47 +0300

    first task

--- routes/indexRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const bookRouter = require('./bookRouter')
-const userRouter = require('./userRouter')
-const router = express.Router()
-
-router.use('/books', bookRouter)
-router.use('/user', userRouter)
-
-module.exports = router
